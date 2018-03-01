b13627adf63b5f21184398b58a9e87347c6f513d
Ben Iofel <iofelben@gmail.com>
2018-02-28 19:45:29 -0500

    Initial commit

---
+++ infer.ml
@@ -0,0 +1,37 @@
+open Unification
+open Subs
+open Ast
+
+
+type 'a error = OK of 'a | Error of string
+
+type typing_judgement = subst*expr*texpr
+
+
+let rec infer' (e:expr) (n:int): (int*typing_judgement) error =
+  match e with
+  | _ -> failwith "infer': undefined"
+
+
+
+
+
+let infer_type (AProg e) =
+  match infer' e 0 with
+  | OK (_, tj) -> string_of_typing_judgement tj
+  | Error s -> "Error! "^ s
+
+
+
+let parse s =
+  let lexbuf = Lexing.from_string s in
+  let ast = Parser.prog Lexer.read lexbuf in
+  ast
+
+
+(* Interpret an expression *)
+let inf (e:string) : string =
+  e |> parse |> infer_type 
+
+let test (n:int) : string =
+  Examples.expr n |> parse |> infer_type 
---
+++ subs.ml
@@ -0,0 +1,4 @@
+open Ast
+
+type subst = (string,Ast.texpr) Hashtbl.t
+
