b48965f039f8173845b03a8f90a5e3ad97c51115
Ben Iofel <iofelben@gmail.com>
2018-03-24 19:40:11 -0400

    indentation

--- subs.ml
+++ subs.ml
@@ -11,27 +11,27 @@ let remove = Hashtbl.remove
 let lookup = Hashtbl.find_opt
 
 let apply_to_texpr set = function
-    | x -> x
+  | x -> x
 
 let apply_to_expr set = function
-    | x -> x
+  | x -> x
 
 let apply_to_env set env = ()
 
 let string_of_subs set =
-    Hashtbl.fold (fun k v acc ->
-        k ^ " -> " ^ Ast.string_of_texpr(v) ^ ", " ^ acc
-    ) set ""
+  Hashtbl.fold (fun k v acc ->
+    k ^ " -> " ^ Ast.string_of_texpr(v) ^ ", " ^ acc
+  ) set ""
 
 let domain set =
-    Hashtbl.fold (fun k v acc -> k :: acc) set []
+  Hashtbl.fold (fun k v acc -> k :: acc) set []
 
 let rec join sets : subst =
-    let combine a b : subst = begin
-        Hashtbl.fold (fun k v acc -> Hashtbl.add a k v) b ();
-        a
-    end in
-    match sets with
-    | [] -> create ()
-    | [x] -> x
-    | x :: xs -> combine x (join xs)
+  let combine a b : subst = begin
+    Hashtbl.fold (fun k v acc -> Hashtbl.add a k v) b ();
+    a
+  end in
+  match sets with
+  | [] -> create ()
+  | [x] -> x
+  | x :: xs -> combine x (join xs)
