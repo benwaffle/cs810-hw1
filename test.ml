27895911340438ee46b07fc03e64dbdcfbd7aeba
Ben Iofel <iofelben@gmail.com>
2018-03-24 23:05:20 -0400

    simplify

--- test.ml
+++ test.ml
@@ -4,7 +4,7 @@ let () =
   (* for i = 1 to 24 do
     printf "====================\nTest: %s\nType: %s\n" (Examples.expr i) (Infer.test i)
   done *)
-  let x = Subs.create () in begin
+  let x = Subs.create () in
     print_string "adding ayy = g -> bool\n";
     Subs.extend x "ayy" (Ast.FuncType ((Ast.VarType "g"), Ast.BoolType));
     printf "%s\n" (Subs.string_of_subs x);
@@ -12,4 +12,3 @@ let () =
     Subs.extend x "g" Ast.IntType;
     printf "%s\n" (Subs.string_of_subs x);
     printf "%s\n" (Ast.string_of_texpr (Subs.apply_to_texpr x @@ VarType("ayy")))
-  end
